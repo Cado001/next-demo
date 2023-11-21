dedc050f6d609266a2d268217b5fbd2385666218
Micado Pierre <Biml16gb@gmail.com>
2023-11-21 16:03:18 -0500

    next context

---
+++ next-context/app/page.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        
+        <div className="">
+          <Link
+          href="/login"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Login{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </Link>
+        </div>
+      </div>
+
+     
+   
+    </main>
+  )
+}
